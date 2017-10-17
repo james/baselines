f0bb95b36556d88a7f40f0244e913d1009848c1d
ViktorM <victor.makoviychuk@gmail.com>
2017-10-17 05:27:16 -0700

    Added save/load to the ACKTR agent. Started working on the parallel implementation. Todo: argparse arguments to create policy and value network of any size.

--- baselines/acktr/acktr_cont_parallel.py
+++ baselines/acktr/acktr_cont_parallel.py
@@ -49,8 +49,59 @@ def rollout(env, policy, max_pathlength, animate=False, obfilter=None):
             "action_dist": np.array(ac_dists), "logp" : np.array(logps)}
 
 
-def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, logdir, agentName, num_timesteps,
-    animate=False, callback=None, desired_kl=0.002):
+def rollout_parallel(env, policy, max_pathlength, animate=False, obfilter=None):
+    """
+    Simulate the env and policy for max_pathlength steps
+    """
+  #  ob = env.reset()
+    ob = env.reset_parallel()
+    prev_ob = np.float32(np.zeros(ob.shape))
+    if obfilter: ob = obfilter(ob)
+    terminated = False
+
+    obs = []
+    acs = []
+    ac_dists = []
+    logps = []
+    rewards = []
+
+    mb_obs, mb_rewards, mb_actions, mb_values, mb_dones = [],[],[],[],[]
+    for _ in range(max_pathlength):
+        if animate:
+            env.render()
+        state = np.concatenate([ob, prev_ob], -1)
+        obs.append(state)
+        ac, ac_dist, logp = policy.acact_parallelt(state) # add ?
+        acs.append(ac)
+        ac_dists.append(ac_dist)
+        logps.append(logp)
+        prev_ob = np.copy(ob)
+        scaled_ac = env.action_space.low + (ac + 1.) * 0.5 * (env.action_space.high - env.action_space.low)
+        scaled_ac = np.clip(scaled_ac, env.action_space.low, env.action_space.high)
+        
+        ob, rew, done, _ = env.step_parallel(scaled_ac)
+        #batch of steps to batch of rollouts
+    #    mb_obs = np.asarray(mb_obs, dtype=np.float32).swapaxes(1, 0).reshape(self.batch_ob_shape)
+    #    mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
+    #    mb_actions = np.asarray(mb_actions, dtype=np.int32).swapaxes(1, 0)
+    #    mb_values = np.asarray(mb_values, dtype=np.float32).swapaxes(1, 0)
+    #    mb_dones = np.asarray(mb_dones, dtype=np.bool).swapaxes(1, 0)
+    #    mb_masks = mb_dones[:, :-1]
+    #    mb_dones = mb_dones[:, 1:]
+
+        if obfilter: ob = obfilter(ob)
+        rewards.append(rew)
+        if done:
+            terminated = True
+            break
+    return {"observation" : np.array(obs), "terminated" : terminated,
+            "reward" : np.array(rewards), "action" : np.array(acs),
+            "action_dist": np.array(ac_dists), "logp" : np.array(logps)}
+
+
+def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, agentName, logdir, num_timesteps,
+    animate=False, num_parallel=1, callback=None, desired_kl=0.002, max_grad_norm=1.0, save_interval=10, 
+    lrschedule='linear'):
 
     obfilter = ZFilter(env.observation_space.shape)
 
@@ -59,7 +110,7 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, logdir, agen
     inputs, loss, loss_sampled = policy.update_info
     optim = kfac.KfacOptimizer(learning_rate=stepsize, cold_lr=stepsize*(1.0 - 0.9), momentum=0.9, kfac_update=2,\
                                 epsilon=1e-2, stats_decay=0.99, async=1, cold_iter=1,
-                                weight_decay_dict=policy.wd_dict, max_grad_norm=None)
+                                weight_decay_dict=policy.wd_dict, max_grad_norm=max_grad_norm)
     pi_var_list = []
     for var in tf.trainable_variables():
         if "pi" in var.name:
@@ -83,7 +134,6 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, logdir, agen
         ob_filter_path = os.path.join(os.path.abspath(logdir), "{}-{}".format('obfilter', resume))
         with open(ob_filter_path, 'rb') as ob_filter_input:
             obfilter = pickle.load(ob_filter_input)
-            print("Loaded observation filter")
     iters_so_far = resume
 
     logF = open(os.path.join(logdir, 'log.txt'), 'a')
@@ -99,6 +149,7 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, logdir, agen
         timesteps_this_batch = 0
         paths = []
         while True:
+#            path = rollout(env, policy, max_pathlength, animate=(len(paths)==0 and (i % 10 == 0) and animate), obfilter=obfilter)
             path = rollout(env, policy, max_pathlength, animate=(len(paths)==0 and (iters_so_far % 10 == 0) and animate), obfilter=obfilter)
             paths.append(path)
             n = pathlength(path)
@@ -160,7 +211,6 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, logdir, agen
         logF2.flush()
      #   logStats.flush()
 
-        save_interval = 10
         if save_interval and (iters_so_far % save_interval == 0 or iters_so_far == 1):
             saver.save(tf.get_default_session(), os.path.join(logdir, agentName), global_step=iters_so_far)
             ob_filter_path = os.path.join(os.path.abspath(logdir), "{}-{}".format('obfilter', iters_so_far))
@@ -171,3 +221,5 @@ def learn(env, policy, vf, gamma, lam, timesteps_per_batch, resume, logdir, agen
             callback()
         logger.dump_tabular()
         iters_so_far += 1
+
+
